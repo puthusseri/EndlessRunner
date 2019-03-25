4803230535393650be25711cadc6ef6051f8fc9d
puthusseri <vyshakputhusseri@gmail.com>
2019-03-25 18:09:55 +0530

    The player can move left and right

--- Assets/Scripts/PlayerMoter.cs
+++ Assets/Scripts/PlayerMoter.cs
@@ -6,6 +6,9 @@ public class PlayerMoter : MonoBehaviour
 {
 	private CharacterController controller;
 	private float speed = 5.0f;
+	private Vector3 moveVector;
+	private float verticalVelocity = 0.0f;
+	private float gravity = 12.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,21 @@ public class PlayerMoter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        controller.Move((Vector3.forward * speed ) * Time.deltaTime);
+		if(controller.isGrounded)
+		{
+			verticalVelocity = -0.5f;
+		}
+		else
+		{
+				verticalVelocity-= gravity *Time.deltaTime;
+		}
+		moveVector = Vector3.zero;
+		// x for left and right
+		//y for up and down
+		//z for frwd and backward
+		moveVector.x = Input.GetAxisRaw("Horizontal")* speed; 
+		moveVector.y = verticalVelocity;
+		moveVector.z = speed;
+        controller.Move(moveVector * Time.deltaTime);
     }
 }
