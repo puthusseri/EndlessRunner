0dc42b65bf6a29933dd96fc90818e366ee0e197b
puthusseri <vyshakputhusseri@gmail.com>
2019-03-25 17:53:47 +0530

    The player can automatically move

---
+++ Assets/Scripts/PlayerMoter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoter : MonoBehaviour
+{
+	private CharacterController controller;
+	private float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move((Vector3.forward * speed ) * Time.deltaTime);
+    }
+}
