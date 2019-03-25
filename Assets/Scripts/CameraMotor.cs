8e2745e0f7d7b97e6eca6aa5b4d2d40b3a8c7cbc
puthusseri <vyshakputhusseri@gmail.com>
2019-03-25 22:08:43 +0530

    Build the floar plan

--- Assets/Scripts/CameraMotor.cs
+++ Assets/Scripts/CameraMotor.cs
@@ -6,16 +6,35 @@ public class CameraMotor : MonoBehaviour
 {
 	private Transform lookAt;
 	private Vector3 startOffset;
+	private Vector3 moveVector;
+
+	
+	private float trasition = 0.0f;
+	private float animationDuration = 2.0f;
+	private Vector3 animationOffset = new Vector3(0,5,5);
     // Start is called before the first frame update
     void Start()
     {
      lookAt = GameObject.FindGameObjectWithTag("Player").transform;   
-	startOffset = transform.position - lookAt.position;
+	 startOffset = transform.position - lookAt.position;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = lookAt.position + startOffset;
-    }
+        moveVector = lookAt.position + startOffset;
+		moveVector.x = 0;
+		moveVector.y = Mathf.Clamp(moveVector.y,3,5);
+		if(trasition > 1.0f)
+		{
+			transform.position = moveVector;
+		}
+		else
+		{
+			//Animation at the start;
+			transform.position = Vector3.Lerp(moveVector + animationOffset , moveVector,trasition);
+			trasition += Time.deltaTime  * 1 / animationDuration;
+		}
+		transform.position = moveVector;
+	}
 }
