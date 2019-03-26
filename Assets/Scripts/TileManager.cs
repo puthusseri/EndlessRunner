c1bd4b8ccda72e55d7a49f90cf145a5c6afb1e54
puthusseri <vyshakputhusseri@gmail.com>
2019-03-26 17:47:07 +0530

    The floor will spawn automatically upto infinity , next add the feture to remove the unwanted floors

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
+	public GameObject[] tilePrefab;
+	private Transform playerTransform;
+	private float spawnZ = 0.0f;
+	private float tileLength = 23.0f;
+	private int amtTilesOnScreen = 6;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+		for(int i =0;i<amtTilesOnScreen; i++)
+		{
+			SpawnTile();
+		}
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+       if(playerTransform.position.z > (spawnZ = amtTilesOnScreen * tileLength))
+	   {
+		   SpawnTile();
+	   }
     }
+	private void SpawnTile(int prefbIndex = -1) {
+	
+		GameObject go;
+		go = Instantiate(tilePrefab [0]) as GameObject;
+		go.transform.SetParent(transform);
+		go.transform.position = Vector3.forward * spawnZ;
+		spawnZ += tileLength;
+	}
+		
 }
