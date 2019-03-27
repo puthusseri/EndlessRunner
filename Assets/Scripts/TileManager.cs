fcac4e41af96b6f8f983472837d58e14ce43e64a
puthusseri <vyshakputhusseri@gmail.com>
2019-03-27 08:11:53 +0530

    Partial error fixing only in the dynamic allocation of the floor

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,36 +4,40 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-	public GameObject[] tilePrefab;
+	public GameObject[] tilePrefabs;
 	private Transform playerTransform;
 	private float spawnZ = 0.0f;
-	private float tileLength = 23.0f;
-	private int amtTilesOnScreen = 6;
+	private float titleLength = 23.0f;
+	private int amnTilesOnScreen = 7;
+	
     // Start is called before the first frame update
     private void Start()
     {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-		for(int i =0;i<amtTilesOnScreen; i++)
+		playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+		for(int i=0;i<amnTilesOnScreen;i++)
 		{
 			SpawnTile();
 		}
-    }
+
+	}
 
     // Update is called once per frame
     private void Update()
     {
-       if(playerTransform.position.z > (spawnZ = amtTilesOnScreen * tileLength))
-	   {
-		   SpawnTile();
-	   }
+		if(playerTransform.position.z > (spawnZ - amnTilesOnScreen * titleLength))
+		{
+		}
+		
+        
     }
-	private void SpawnTile(int prefbIndex = -1) {
-	
+	private void SpawnTile(int prefabIndex = -1) 
+	{
 		GameObject go;
-		go = Instantiate(tilePrefab [0]) as GameObject;
+		go = Instantiate(tilePrefabs [0]) as GameObject;
 		go.transform.SetParent(transform);
 		go.transform.position = Vector3.forward * spawnZ;
-		spawnZ += tileLength;
-	}
+		spawnZ += titleLength;
 		
+	}
 }
