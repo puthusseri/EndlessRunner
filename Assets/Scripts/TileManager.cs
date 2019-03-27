5c69e38eb2469155c0bcbceca432a61f86784d7e
puthusseri <vyshakputhusseri@gmail.com>
2019-03-27 08:39:51 +0530

    Erroes fixed upto the random floor generation and the deletion of used bridge

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -6,18 +6,26 @@ public class TileManager : MonoBehaviour
 {
 	public GameObject[] tilePrefabs;
 	private Transform playerTransform;
-	private float spawnZ = 0.0f;
+	private float spawnZ = -6.0f;
 	private float titleLength = 23.0f;
 	private int amnTilesOnScreen = 7;
+	private float safeZone = 25.0f;
+	private int lastPrefabIndex = 0;
+	
+	private List<GameObject> activeTiles;
 	
     // Start is called before the first frame update
     private void Start()
     {
+		activeTiles = new List<GameObject>();
 		playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 
 		for(int i=0;i<amnTilesOnScreen;i++)
 		{
-			SpawnTile();
+			if(i<2)
+				SpawnTile(0);
+			else
+				SpawnTile();
 		}
 
 	}
@@ -25,8 +33,11 @@ public class TileManager : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-		if(playerTransform.position.z > (spawnZ - amnTilesOnScreen * titleLength))
+		if(playerTransform.position.z - safeZone > (spawnZ - amnTilesOnScreen * titleLength))
 		{
+			SpawnTile();
+			DeleteTile();
+			
 		}
 		
         
@@ -34,10 +45,32 @@ public class TileManager : MonoBehaviour
 	private void SpawnTile(int prefabIndex = -1) 
 	{
 		GameObject go;
-		go = Instantiate(tilePrefabs [0]) as GameObject;
+		if(prefabIndex == -1)
+			go = Instantiate(tilePrefabs [RandomPrefabindex()]) as GameObject;
+		else
+			go = Instantiate(tilePrefabs[prefabIndex]) as GameObject;
 		go.transform.SetParent(transform);
 		go.transform.position = Vector3.forward * spawnZ;
 		spawnZ += titleLength;
+		activeTiles.Add(go);
 		
+	}
+	private void DeleteTile()
+	{
+		  Destroy(activeTiles[0]);
+		  activeTiles.RemoveAt(0);
+	}
+	private int RandomPrefabindex()
+	{
+		if(tilePrefabs.Length <=1)
+			return 0;
+		int randomIndex = lastPrefabIndex;
+		while(randomIndex == lastPrefabIndex)
+		{
+			randomIndex = Random.Range(0,tilePrefabs.Length);
+			
+		}
+		lastPrefabIndex = randomIndex;
+		return randomIndex;
 	}
 }
