1e66a15413ba60021e918ff26d7dfc0b7e6a76d1
puthusseri <vyshakputhusseri@gmail.com>
2019-04-05 20:35:15 +0530

    started the script for calculating the score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour
+{
+	private float score =0.0f;
+	
+	public Text scoreText; 
+    // Start is called before the first frame update
+    void Start()
+    {
+		//scoreText = 0;     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		score += Time.deltaTime;
+		scoreText.text = ((int)score).ToString();
+        
+		//Increase the  speed of the player after ever 30 seconds
+    }
+}
